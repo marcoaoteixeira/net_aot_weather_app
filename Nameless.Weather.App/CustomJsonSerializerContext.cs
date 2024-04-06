522226f76e2aa86a35484ab6681176fccbb00f1b
Marco Teixeira <marcoaoteixeira@gmail.com>
2024-04-06 11:56:17 +0100

    missing files.

---
+++ Nameless.Weather.App/CustomJsonSerializerContext.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Nameless.Weather.App {
+    [JsonSerializable(typeof(WeatherForecast[]))]
+    public partial class CustomJsonSerializerContext : JsonSerializerContext {
+    }
+}
